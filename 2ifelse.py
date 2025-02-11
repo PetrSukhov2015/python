2213850f84c8f45fd6e8129a8ae967236abc363a
petr.sukhov <petr.sukhov@iva-tech.ru>
2025-02-11 15:55:37 +0300

    add second file

---
+++ 2ifelse.py
@@ -0,0 +1,17 @@
+# Простой пример
+age = 18
+if age >= 18:
+    print("Вы совершеннолетний")
+else:
+    print("Вы несовершеннолетний")
+
+# Пример с elif
+score = 85
+if score >= 90:
+    print("Отлично!")
+elif score >= 70:
+    print("Хорошо!")
+elif score >= 69:
+    print("Хорошо!")
+else:
+    print("Попробуйте еще раз")
